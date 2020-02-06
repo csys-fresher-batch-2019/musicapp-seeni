76a47dc79aa60ae50dfdf0c636867149b5139d1f
Seenivasan <vnseeni1999@gmail.com>
2020-02-06 09:34:58 +0530

    Update AccountTab5Tab1DAOImpl.java

--- src/main/java/add/AccountTab5Tab1DAOImpl.java
+++ src/main/java/add/AccountTab5Tab1DAOImpl.java
@@ -11,16 +11,17 @@ public class AccountTab5Tab1DAOImpl implements AccountTab5Tab1DAO{
 	public List<String> accountInfo(String str) throws Exception{
 		List<String> li=null;
 		Connection con=null;
-		
+		PreparedStatement pst=null;
+		ResultSet rs=null;
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
 			con=DriverManager.getConnection("jdbc:oracle:thin:@CSLH2013:1521:XE", "system", "oracle");
 			int premiumAmount=400;
 			String sql="select username from userlogin where user_id=(select user_id from account_info where wants_to_premium=? and balance>="+premiumAmount+")";
-			PreparedStatement pst=con.prepareStatement(sql);
+			pst=con.prepareStatement(sql);
 			System.out.println(sql);
 			pst.setString(1, str);
-			ResultSet rs=pst.executeQuery();
+			rs=pst.executeQuery();
 			li = new ArrayList<String>();
 			while(rs.next()) {
 				li.add(rs.getString("username"));
@@ -32,7 +33,9 @@ public class AccountTab5Tab1DAOImpl implements AccountTab5Tab1DAO{
 			e.printStackTrace();
 		}finally {
 			try {
+				
 				if(con!=null) {
+					rs.close();
 					pst.close();
 					con.close();
 				}
