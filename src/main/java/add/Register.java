378f3cb0194d761883f80a1b2123ead4a0e6d257
Seenivasan N <seenivasan.nagarajan@global.chainsys.com>
2020-02-06 16:51:17 +0530

    last

--- src/main/java/add/Register.java
+++ src/main/java/add/Register.java
@@ -7,26 +7,10 @@ import java.util.Scanner;
 
 public class Register {
 	public boolean register(Userlogin a) throws Exception {
-		/*Scanner sc=new Scanner(System.in);
-		System.out.println("Enter user Id ");
-		int userId=sc.nextInt();
-		a.userId=userId;
-		System.out.println("Enter user name ");
-		String userName=sc.next();
-		a.userName=userName;
-		System.out.println("Enter EmailID ");
-		String emailId=sc.next();
-		a.emailId=emailId;
-		System.out.println("Enter Password ");
-		String password=sc.next();
-		a.password=password;
-		System.out.println("Enter Mobile Number ");
-		Long mobileNo=sc.nextLong();
-		a.mobileNo=mobileNo;
-		*/
+		
 		Class.forName("oracle.jdbc.driver.OracleDriver");
 		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@CSLH2013:1521:XE", "system", "oracle");
-		String sql="insert into userlogin(user_id,username,email_id,password,mobile_no) values("+a.userId+",'"+a.userName+"','"+a.emailId+"','"+a.password+"',"+a.mobileNo+")";
+		String sql="insert into userlogin(user_id,username,email_id,password,mobile_no) values("+a.getUserId()+",'"+a.getUserName()+"','"+a.getEmailId()+"','"+a.getPassword()+"',"+a.getMobileNo()+")";
 		boolean insertion=true;
 		if (insertion) {
 			Statement st = con.createStatement();
