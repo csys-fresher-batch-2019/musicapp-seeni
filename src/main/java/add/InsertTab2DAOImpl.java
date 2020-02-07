939f36975b18610c2c51901f4193c1e46efd1a51
Seenivasan N <seenivasan.nagarajan@global.chainsys.com>
2020-02-07 09:32:59 +0530

    new bugs

--- src/main/java/add/InsertTab2DAOImpl.java
+++ src/main/java/add/InsertTab2DAOImpl.java
@@ -6,38 +6,38 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class InsertTab2DAOImpl implements InsertTab2DAO{
-	public void insertSong(SongList songList) throws ClassNotFoundException, SQLException  {
+	public void insertSong(SongList sl) throws ClassNotFoundException, SQLException  {
 		Connection con=Connection1.connection();
 		String sql ="insert into song_list(song_number,song_name,music_director,lyricist,singers,movie_name) "
 				+ "values(?,?,?,?,?,?)";
 		PreparedStatement pst=con.prepareStatement(sql);
-		songList=new SongList();
+		sl=new SongList();
 		Scanner sc=new Scanner(System.in);
 		Logger.info("Enter song number ");
 		int number=sc.nextInt();
 		sc.nextLine();
-		songList.setSongNumber(number);
-		pst.setInt(1,songList.getSongNumber());
+		sl.setSongNumber(number);
+		pst.setInt(1,sl.getSongNumber());
 		Logger.info("Enter song name ");
 		String sname=sc.nextLine();
-		songList.setSongName(sname);
-		pst.setString(2, songList.getSongName());
+		sl.setSongName(sname);
+		pst.setString(2, sl.getSongName());
 		Logger.info("Enter music director");
 		String mdir=sc.nextLine();
-		songList.setMusicDirector(mdir);
-		pst.setString(3, songList.getMusicDirector());
+		sl.setMusicDirector(mdir);
+		pst.setString(3, sl.getMusicDirector());
 		Logger.info("Enter Lyricist name ");
 		String lyricist=sc.nextLine();
-		songList.setLyricist(lyricist);
-		pst.setString(4, songList.getLyricist());
+		sl.setLyricist(lyricist);
+		pst.setString(4, sl.getLyricist());
 		Logger.info("Enter singers name ");
 		String singers=sc.nextLine();
-		songList.setSingers(singers);
-		pst.setString(5, songList.getSingers());
+		sl.setSingers(singers);
+		pst.setString(5, sl.getSingers());
 		Logger.info("Enter movie name ");
 		String mname=sc.nextLine();
-		songList.setMovieName(mname);
-		pst.setString(6, songList.getMovieName());
+		sl.setMovieName(mname);
+		pst.setString(6, sl.getMovieName());
 		int rows=pst.executeUpdate();
 		pst.close();
 		Logger.info(sql);
