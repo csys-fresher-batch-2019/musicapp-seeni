142699f28bc861f36a2540c0a7fb786b21f3dd45
Seenivasan N <seenivasan.nagarajan@chainsys.com>
2020-02-21 14:38:15 +0530

    After boolean changed

--- src/main/java/com/chainsys/OtherClass/Register.java
+++ src/main/java/com/chainsys/OtherClass/Register.java
@@ -7,7 +7,7 @@ public class Register{
 	public  boolean register(Userlogin ul) throws ClassNotFoundException, SQLException {
 		try(Connection con=Connection1.connection();){
 		String sql="insert into userlogin(user_id,username,email_id,password,mobile_no) values("+ul.getUserId()+",'"+ul.getUserName()+"','"+ul.getEmailId()+"','"+ul.getPassword()+"',"+ul.getMobileNo()+")";
-		boolean sql1=true;
+		boolean sql1 = true;
 		if(sql1) {
 			try(Statement st=con.createStatement();){
 			int row=st.executeUpdate(sql);
@@ -15,9 +15,7 @@ public class Register{
 			sql1=true;
 			}
 		}
-		else {
-			sql1=false;
-		}return sql1;}
+		return sql1;}
 	}
 }
 
